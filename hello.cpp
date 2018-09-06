200ce1f0851d0b755522d3079871a080c9312463
jdrhodes@mail.csuchico.edu <xuyisab98@Jesuss-MacBook-Air-3.local>
2018-09-05 21:30:29 -0700

    Changes the Greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+#include <iostream> //Author: Jeremy Rhodes
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Jeremy. What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
